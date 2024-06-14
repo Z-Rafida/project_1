d288947c0ccafa541adb2d49fbb77396be0b3db7
Rafida <zakariarafida81@gmail.com>
2024-06-14 10:29:39 +0100

    second update

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const btn1 = document.getElementById('btn1')
+btn1.onclick = function() {
+    alert('BOOK US NOW!!')
+}
+
+const btn2 = document.getElementById('btn2')
+btn2.onclick = function() {
+    alert('BOOK US NOW!!')
+}
+
+const choose_plan_btn = document.getElementById('choose_plan_btn')
+choose_plan_btn.onclick = function() {
+    alert('Thanks for choosing a plan!!')
+}
